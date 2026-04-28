5327ca5c825c87eb0acff1886cf1c9f469e88a5c
JaiDhundhara <jayantdhundharra3030@gmail.com>
2026-04-28 22:22:19 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int cnt=1;
+        int cele=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==cele){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+
+            if(cnt<0) cele=nums[i];
+        }
+        return cele;
+    }
+}
